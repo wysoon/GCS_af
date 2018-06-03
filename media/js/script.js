18037168197e3b30e04bcf41ec12c213b18b4c4a
Hamed Rohani <hamedrohani@Hameds-MacBook-Pro.local>
2018-06-02 19:55:52 -0700

    navbar

--- media/js/script.js
+++ media/js/script.js
@@ -18,6 +18,15 @@ $(document).ready(function(){
 
 
 
+  var header = document.getElementById("nnbar");
+var btns = header.getElementsByClassName("nnactive");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
 window.onscroll = function() {myFunction()};
 
